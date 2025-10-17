78607a06be4b1a8d658e0f9e4fd45294786b9db8
Student <nguyenanthonyd4@gmail.com>
2025-10-16 20:09:22 -0700

    Copy of original

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        HomeScreen.homeScreen(); // Called my menu here
+
+
+
+    }
+}
+
+// Application Requirements
+//Your application must include several screens with the listed features in order to
+//be considered complete:
+//• Home Screen
+//o The home screen should give the user the following options. The
+//application should continue to run until the user chooses to exit.
+//§ D) Add Deposit - prompt user for the deposit information and
+//save it to the csv file
+//§ P) Make Payment (Debit) - prompt user for the debit
+//information and save it to the csv file
+//§ L) Ledger - display the ledger screen
+//§ X) Exit - exit the application
+//• Ledger - All entries should show the newest entries first
+//o A) All - Display all entries
+//o D) Deposits - Display only the entries that are deposits into the
+//account
+//o P) Payments - Display only the negative entries (or payments)
+//o R) Reports - A new screen that allows the user to run pre-defined
+//reports or to run a custom search
+//§ 1) Month To Date
+//§ 2) Previous Month
+//§ 3) Year To Date
+//§ 4) Previous Year
+//§ 5) Search by Vendor - prompt the user for the vendor name
+//and display all entries for that vendor
+//§ 0) Back - go back to the Ledger page
+//o H) Home - go back to the home page
