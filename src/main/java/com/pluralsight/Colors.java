7f0855d62d6fcc237af575decb399122ae9b2087
Student <nguyenanthonyd4@gmail.com>
2026-01-16 12:27:53 -0800

    another one

---
+++ src/main/java/com/pluralsight/Colors.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class Colors {
+
+
+        // Reset
+        public static final String RESET = "\u001B[0m";
+
+        // Regular Cosmetics.Colors
+        public static final String BLACK = "\u001B[30m";
+        public static final String RED = "\u001B[31m";
+        public static final String GREEN = "\u001B[32m";
+        public static final String YELLOW = "\u001B[33m";
+        public static final String BLUE = "\u001B[34m";
+        public static final String PURPLE = "\u001B[35m";
+        public static final String CYAN = "\u001B[36m";
+        public static final String WHITE = "\u001B[37m";
+
+        // Bold
+        public static final String BOLD = "\u001B[1m";
+    }
+
+
